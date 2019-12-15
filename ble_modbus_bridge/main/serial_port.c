fb7a483051a6a65fe43bee6ed0cb43b1ef4c066f
Biju Nair <biju.nair@infinicomm.io>
2019-12-15 18:23:03 +0530

    Added tasks for BLE

--- ble_modbus_bridge/main/serial_port.c
+++ ble_modbus_bridge/main/serial_port.c
@@ -26,12 +26,7 @@
 #include "tcpip_adapter.h"
 #include "lwip/sockets.h"
 #include "lwip/netdb.h"
-
-#define ECHO_TEST_TXD  (GPIO_NUM_17)
-#define ECHO_TEST_RXD  (GPIO_NUM_16)
-
-#define ECHO_TEST_RTS  (UART_PIN_NO_CHANGE)
-#define ECHO_TEST_CTS  (UART_PIN_NO_CHANGE)
+#include "peripheral.h"
 
 static const char* TAG = "uart_select_example";
 
@@ -54,6 +49,11 @@ static void deinit_uart()
 
 static int init_uart()
 {
+
+    /*
+     * Initialize the UART port & open the device here.
+     */
+
     uart_param_config(UART_NUM_2, &uart_config);
     uart_set_pin(UART_NUM_2, ECHO_TEST_TXD, ECHO_TEST_RXD, 
                  ECHO_TEST_RTS, ECHO_TEST_CTS);
@@ -62,7 +62,7 @@ static int init_uart()
     printf(" INFO : Opening Serial Port 2 \n");
 
     if ((uart_fd = open("/dev/uart/2", O_RDWR | O_NONBLOCK)) == -1) {
-        ESP_LOGE(TAG, "Cannot open UART1");
+        printf(" ERROR : Cannot open UART1");
         deinit_uart();
         return (1);
     }
@@ -80,9 +80,9 @@ static void check_and_uart_data(int fd, const fd_set *rfds, const char *src_msg)
     if (FD_ISSET(fd, rfds)) {
         if ((read_bytes = read(fd, buf, sizeof(buf)-1)) > 0) {
             buf[read_bytes] = '\0';
-            ESP_LOGI(TAG, "%d bytes were received through %s: %s", read_bytes, src_msg, buf);
+            printf( " INFO : %d bytes were received through %s: %s", read_bytes, src_msg, buf);
         } else {
-            ESP_LOGE(TAG, "%s read error", src_msg);
+            printf(" ERROR : %s read error", src_msg);
         }
     }
 }
