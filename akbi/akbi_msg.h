ba2c0093c1a722d0afa84f517792391576ccf24e
sathishchand <sathish.chandran@infinicomm.io>
2020-01-01 10:01:56 +0530

    Update akbi_msg.h

--- akbi/akbi_msg.h
+++ akbi/akbi_msg.h
@@ -1,37 +1,37 @@
 #define CURRENT_FIRMWARE_VERSION "1.0(1)"
 #define BLE_DEBUG
 
-#define SER_NO_SIZE                   8
-#define PASS_SIZE                     15
-#define MOB_NO_SIZE                   10
-#define MOB_NAME_SIZE                 15
-#define IMEI_NO_SIZE                  15
-#define BLE_APP_TYPE_ID_SIZE          1
-#define BLE_APP_ID_SIZE               2
-#define BLE_APP_AUTH_TOKEN_SIZE       2
-#define FILE_NAME_SIZE                30
-#define PERSONAL_VOICE_MESSAGES_COUNT 6
-#define DEFAULT_EMERGENCY_NUMBER      911
-#define DEFAULT_EMERGENCY_NUMBER_SIZE 3
-#define SSID_SIZE                     15
-#define NETWORK_KEY_SIZE              15
-#define LOCATION_COUNT                10
-#define LAT_LONG_SIZE                 11
-#define TIMESTAMP_SIZE                10
-#define FW_UPGRADE_COUNT              10
-#define FW_VERSION_STRING_SIZE        10
-#define DATE_SIZE                     10
-#define MOB1_VER_SIZE                 10
-#define ACTIVATIONS_COUNT             10
-#define WIFI_CONNECTIONS_COUNT        10
+#define SER_NO_SIZE                     8
+#define PASS_SIZE                       15
+#define MOB_NO_SIZE                     10
+#define MOB_NAME_SIZE                   15
+#define ANDROID_ID_OR_UUID_SIZE         15
+#define BLE_APP_TYPE_ID_SIZE            1
+#define BLE_APP_ID_SIZE                 1
+#define BLE_APP_AUTH_TOKEN_SIZE         2
+#define FILE_NAME_SIZE                  30
+#define PERSONAL_VOICE_MESSAGES_COUNT   6
+#define DEFAULT_EMERGENCY_NUMBER        911
+#define DEFAULT_EMERGENCY_NUMBER_SIZE   3
+#define SSID_SIZE                       15
+#define NETWORK_KEY_SIZE                15
+#define LOCATION_COUNT                  10
+#define LAT_LONG_SIZE                   11
+#define TIMESTAMP_SIZE                  10
+#define FW_UPGRADE_COUNT                10
+#define FW_VERSION_STRING_SIZE          10
+#define DATE_SIZE                       10
+#define MOB1_VER_SIZE                   10
+#define ACTIVATIONS_COUNT               10
+#define WIFI_CONNECTIONS_COUNT          10
 
-#define CID_REGISTER                  0x01
-#define CID_LOGIN                     0x02
+#define CID_REGISTER                    0x01
+#define CID_LOGIN                       0x02
 
-#define DID_REGISTER_PASSWORD         0x01
-#define DID_REGISTER_MOB_NO           0x02
-#define DID_REGISTER_MOB_NAME         0x03
-#define DID_REGISTER_IMEI             0x04
+#define DID_REGISTER_PASSWORD           0x01
+#define DID_REGISTER_MOB_NO             0x02
+#define DID_REGISTER_MOB_NAME           0x03
+#define DID_REGISTER_ANDROID_ID_OR_UUID 0x04
 
 const int  BLE_MESSAGE_SIZE               = 30;
 const int  BLE_COMMAND_SIZE               = 26; //BLE_MESSAGE SIZE - Source App Type (1) - Source App ID (2) - Command Id (1)
@@ -41,7 +41,7 @@ const int  BLE_RETURN_RC_SIZE             = 1;
 
 const int  BLE_APP_TYPE_OFFSET            = 0;
 const int  BLE_APP_OFFSET                 = 1;
-const int  BLE_CMD_OFFSET                 = 3;
+const int  BLE_CMD_OFFSET                 = 2;
 const int  BLE_CMD_REG_DATA_TYPE_OFFSET   = 0; //for commands with multiple data types sent as multiple messages
 const int  BLE_CMD_REG_DATA_LEN_OFFSET    = 1; //for commands with multiple data types sent as multiple messages
 const int  BLE_CMD_REG_DATA_VALUE_OFFSET  = 2; //for commands with multiple data types sent as multiple messages
