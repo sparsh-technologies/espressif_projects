19732dc3c77c9fe3f4b38d7c714749da416e980e
Biju Nair <biju.nair@infinicomm.io>
2020-01-29 18:39:07 +0530

    Added code for sending message to CCU

---
+++ akbi/ble_ccu/main/akbi_ccu_api.c
@@ -0,0 +1,64 @@
+/*
+ ****************************************************************************************
+ * akbi_ccu_api.c
+ *
+ * Author    : Biju Nair
+ * Ver       : 1.0
+ * Date      : 28-Jan-2020
+ *
+ * Copyright Infinicomm Solutions (I) Pvt Ltd, 2020
+ *
+ ****************************************************************************************
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#include "akbi_bt_msg.h"
+#include "akbi_msg.h"
+
+int ccu_sent_subg_clear_learning_msg(char *p_tx_buffer, int *length) 
+{
+    BT_CP_PROTOCOL_HDR  *p_protocol_hdr;
+    char                *p;
+
+    p_protocol_hdr = (BT_CP_PROTOCOL_HDR *)p_tx_buffer;
+
+    p_protocol_hdr->opcode   = BT_CP_OPCODE_CLEAR_SUB_G_LEARN;
+    p_protocol_hdr->trans_id = 44;
+    p_protocol_hdr->type     = 0;
+    p_protocol_hdr->length   = 0;
+
+    printf(" Opcode      :  %02x\n",p_protocol_hdr->opcode );
+    printf(" Trans ID    :  %02x\n", p_protocol_hdr->trans_id);
+    printf(" Type        :  %02x\n", p_protocol_hdr->type);
+    printf(" Length      :  %02x\n", p_protocol_hdr->length);
+
+    *length = sizeof(BT_CP_PROTOCOL_HDR) + p_protocol_hdr->length;
+
+    return (0);
+}
+
+int ccu_sent_subg_learning_msg(char *p_tx_buffer, int *length)
+{
+    BT_CP_PROTOCOL_HDR  *p_protocol_hdr;
+    char                *p;
+
+    p_protocol_hdr = (BT_CP_PROTOCOL_HDR *)p_tx_buffer;
+
+    p_protocol_hdr->opcode   = BT_CP_OPCODE_START_SUB_G_LEARN;
+    p_protocol_hdr->trans_id = 44;
+    p_protocol_hdr->type     = 0;
+    p_protocol_hdr->length   = 0;
+
+    printf(" Opcode      :  %02x\n",p_protocol_hdr->opcode );
+    printf(" Trans ID    :  %02x\n", p_protocol_hdr->trans_id);
+    printf(" Type        :  %02x\n", p_protocol_hdr->type);
+    printf(" Length      :  %02x\n", p_protocol_hdr->length);
+
+    *length = sizeof(BT_CP_PROTOCOL_HDR) + p_protocol_hdr->length;
+
+    return (0);
+}
+
