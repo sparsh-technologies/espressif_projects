23df35d7dc91f5d03621d9c3dae9e1c556bf761d
infinicomm <biju.nair@infinicomm.io>
2020-01-31 13:23:12 +0530

    Fixed compilation issue

--- akbi/ble_ccu/main/akbi_ccu_msg_handler.c
+++ akbi/ble_ccu/main/akbi_ccu_msg_handler.c
@@ -3,28 +3,31 @@
 #include <string.h>
 #include "akbi_bt_msg.h"
 #include "akbi_msg.h"
+#include "akbi_ccu_api.h"
 
 AKBI_WIFI_SCAN_REPORT  wifi_scan_report;
 
-void akbi_process_rx_serial_data(char *ccu_msg,int length,char *return_pointer){
+void akbi_process_rx_serial_data(char *ccu_msg,int length,char *return_pointer)
+{
 
-     char *p;
+    char *p;
 
-      BT_CP_PROTOCOL_HDR  *p_protocol_hdr;
+    BT_CP_PROTOCOL_HDR  *p_protocol_hdr;
 
-      p_protocol_hdr = (BT_CP_PROTOCOL_HDR *)ccu_msg;
+    p_protocol_hdr = (BT_CP_PROTOCOL_HDR *)ccu_msg;
 
-      printf(" Opcode      :  %02x\n",p_protocol_hdr->opcode );
-      printf(" Trans ID    :  %02x\n", p_protocol_hdr->trans_id);
-      printf(" Type        :  %02x\n", p_protocol_hdr->type);
-      printf(" Length      :  %02x\n", p_protocol_hdr->length);
+    printf(" Opcode      :  %02x\n",p_protocol_hdr->opcode );
+    printf(" Trans ID    :  %02x\n", p_protocol_hdr->trans_id);
+    printf(" Type        :  %02x\n", p_protocol_hdr->type);
+    printf(" Length      :  %02x\n", p_protocol_hdr->length);
 
-      p = ccu_msg + sizeof(BT_CP_PROTOCOL_HDR);
+    p = ccu_msg + sizeof(BT_CP_PROTOCOL_HDR);
 
-      memcpy(return_pointer+BLE_RET_MSG_SCANNED_SSID_COUNT_OFFSET, p , p_protocol_hdr->length);
+    memcpy(return_pointer+BLE_RET_MSG_SCANNED_SSID_COUNT_OFFSET, p , p_protocol_hdr->length);
 
     switch(p_protocol_hdr->opcode)
     {
+
     case BT_CP_OPCODE_CID_SCAN_WIFI_RESULT :
 
         /*
